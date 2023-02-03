32baef625fc6d0b7e23e2546cc8fa931dda7e45d
abdu <abdumah2021fr@gmail.com>
2023-02-03 22:36:22 +0100

    adderade en animation till menyn så att den kommer smoothly från sidan.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const grayfilter = document.querySelector('.grayfilter');
 menuopn.addEventListener("click", openmenu);
 
 function openmenu() {
-  menu.classList.toggle('hidden');  
+  menu.classList.toggle('open');  
   menuopn.style.zIndex = '11';
   grayfilter.classList.toggle('hidden');
   body.classList.toggle('scroll');
@@ -16,8 +16,10 @@ function openmenu() {
   if (menuopn.classList.contains('rotateO')) {
     menuopn.classList.remove('rotateO');
     menuopn.classList.add('rotateC');
+    menu.classList.remove('open');
   } else {
     menuopn.classList.remove('rotateC');
     menuopn.classList.add('rotateO');
+    menu.classList.add('open');
   }
 }
